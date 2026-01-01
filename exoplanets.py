0d6d3a2e0e06fc154e820741e3e7aa9dd18cef34
Mario Braz <mariobbraz@gmail.com>
2026-01-01 15:59:41 -0300

    Adjust the number display and add docstrings

--- exoplanets.py
+++ exoplanets.py
@@ -9,7 +9,6 @@ response = requests.get(
 
 # Use the NASA API to get information about exoplanets
 def main():
-    """
     get_nasa_data()
     nasa_data_json()
     all_data_csv()
@@ -22,17 +21,24 @@ def main():
     size_exoplanets()
     orbital_period()
     exoplanets_mass()
-    """
     stars_mass()
-    # insolation()
+    insolation()
 
 
 def get_nasa_data():
+    """
+    Get all the data of the NASA exoplanets API in csv format and
+    save it in 'nasa_exoplanets.csv'
+    """
     with open("nasa_exoplanets.csv", "w") as f:
         f.write(response.text)
 
 
 def check_duplicates(reader):
+    """
+    Check if there are duplicates of the explanets and remove them using a
+    new list and a set checking by their name
+    """
     database = []
     set_data = set()
     for row in reader:
@@ -42,8 +48,11 @@ def check_duplicates(reader):
     return database
 
 
-####
 def all_data_csv():
+    """
+    This function clean the duplicates of the API data and save it in
+    'exoplanets.csv'
+    """
     with open("nasa_exoplanets.csv", "r") as f, open("exoplanets.csv", "w") as file:
         reader = csv.DictReader(f)
         database = check_duplicates(reader)
@@ -55,6 +64,10 @@ def all_data_csv():
 
 
 def clean_csv_data():
+    """
+    Clean the data using only relevant data, remove dulplicates and save it in
+    'key_exoplanets.csv'
+    """
     with open("nasa_exoplanets.csv", "r") as f, open("key_exoplanets.csv", "w") as file:
         fieldnames = [
             "pl_name",
@@ -83,6 +96,9 @@ def clean_csv_data():
 
 
 def write_json(reader, file):
+    """
+    Make easier to write from csv to json
+    """
     json_format = []
     for row in reader:
         json_format.append(row)
@@ -92,6 +108,10 @@ def write_json(reader, file):
 
 
 def nasa_data_json():
+    """
+    Get all the data from 'nasa_exoplanets.csv', convert it to json
+    and save it in 'nasa_exoplanets.json'
+    """
     with open("nasa_exoplanets.csv", "r") as f:
         reader = csv.DictReader(f)
         write_json(reader, "nasa_exoplanets.json")
@@ -99,18 +119,29 @@ def nasa_data_json():
 
 #####
 def all_data_json():
+    """
+    Convert the data without duplicates ('exoplanets.csv') from csv to json
+    and save it in 'exoplanets.json'
+    """
     with open("exoplanets.csv", "r") as f:
         reader = csv.DictReader(f)
         write_json(reader, "exoplanets.json")
 
 
 def key_data_json():
+    """
+    Convert the data without duplicates and with key data ('key_exoplanets.csv')
+    from csv to json and save it in 'key_exoplanets.json'
+    """
     with open("key_exoplanets.csv", "r") as f:
         reader = csv.DictReader(f)
         write_json(reader, "key_exoplanets.json")
 
 
 def sort_data(funct):
+    """
+    Sort the exoplanet data using a specified sorting function
+    """
     with open("key_exoplanets.json", "r") as file:
         data = json.load(file)
         sorted_data = sorted(data, key=funct)
@@ -126,6 +157,11 @@ def sort_data(funct):
 
 
 def exoplanets_distance():
+    """
+    Sort and enumerate exoplanets by distance from Earth from 'key_exoplanets.json'
+    and formatted in km, au, light years and parsecs and write the formatted
+    and sorted data to 'exoplanets_distance.txt' (if no data print no value)
+    """
     def funct(x): return float(x['sy_dist']) if x['sy_dist'] else 9999
     data = sort_data(funct)
 
@@ -142,7 +178,7 @@ def exoplanets_distance():
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n"
                     f"\tKM: {km:,.0f} \n"
-                    f"\tAU: {au:,.2f} \n"
+                    f"\tAU: {au:,.0f} \n"
                     f"\tLight Years: {light_years:,.2f} \n"
                     f"\tParsecs: {parsecs:,.2f}\n")
             else:
@@ -150,6 +186,10 @@ def exoplanets_distance():
 
 
 def discovery_year():  # disc_year
+    """
+    Sort and enumerate the data by discovery year, formatted and save it
+    in 'exoplanets_discovery.txt'
+    """
     def funct(x): return float(x['disc_year']) if x['disc_year'] else 9999
     data = sort_data(funct)
 
@@ -165,6 +205,10 @@ def discovery_year():  # disc_year
 
 
 def publication_date():  # disc_pubdate
+    """
+    Sort and enumerate the data by discovery publication date and formatted
+    and save it in 'exoplanets_publication.txt'
+    """
     def funct(
         x): return x['disc_pubdate'] if x['disc_pubdate'] else "9999-12-31"
     data = sort_data(funct)
@@ -181,6 +225,10 @@ def publication_date():  # disc_pubdate
 
 
 def size_exoplanets():  # pl_rade
+    """
+    Sort and enumerate the data by size in radius and formatted in Earth Radius (R⊕)
+    and Radius in km and save it in 'exoplanets_size.txt'
+    """
     def funct(x): return float(x['pl_rade']) if x['pl_rade'] else 9999
     data = sort_data(funct)
 
@@ -191,13 +239,18 @@ def size_exoplanets():  # pl_rade
                 exoplanet_radius = float(exoplanet['pl_rade']) * 6371
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n"
-                    f"\tRadius (R⊕): {float(exoplanet['pl_rade']):,.2f} R⊕ \n"
-                    f"\tRadius (km): {exoplanet_radius:,.0f} km\n")
+                    f"\tRadius (R⊕): {float(exoplanet['pl_rade']):,.2f} \n"
+                    f"\tRadius (km): {exoplanet_radius:,.0f} \n")
             else:
                 file.write(f"{i}) Name: {exoplanet['pl_name']}: \n\tNo data\n")
 
 
 def orbital_period():  # pl_orbper
+    """
+    Sort and enumerate the data by orbital period and formatted in Orbital period (days)
+    and Earth years and save it in 'exoplanets_orbital_period.txt'
+    (if orbital period > 10000 its most likely and estimate and write it)
+    """
     def funct(x): return float(x['pl_orbper']
                                ) if x['pl_orbper'] else 999_999_999_999
     data = sort_data(funct)
@@ -210,14 +263,14 @@ def orbital_period():  # pl_orbper
                     earth_years = float(exoplanet['pl_orbper']) / 365.25
                     file.write(
                         f"{i}) Name: {exoplanet['pl_name']}: \n"
-                        f"\tOrbital Period: {float(exoplanet['pl_orbper']):,.2f} \n"
+                        f"\tOrbital Period: {float(exoplanet['pl_orbper']):,.2f} days \n"
                         f"\tEarth Years: {earth_years:,.4f} \n")
 
                 elif exoplanet['pl_orbper'] and float(exoplanet['pl_orbper']) > 10_000:
                     earth_years = float(exoplanet['pl_orbper']) / 365.25
                     file.write(
                         f"{i}) Name: {exoplanet['pl_name']}: \n"
-                        f"\tOrbital Period: (estimated) {float(exoplanet['pl_orbper']):,.2f} \n"
+                        f"\tOrbital Period: (estimated) {float(exoplanet['pl_orbper']):,.2f} days \n"
                         f"\tEarth Years: (estimated) {earth_years:,.4f} \n")
 
             else:
@@ -226,6 +279,10 @@ def orbital_period():  # pl_orbper
 
 
 def exoplanets_mass():  # pl_masse
+    """
+    Sort and enumerate the data by exoplanet mass and formatted in Earth mass(M⊕) and
+    mass in tons and save it in 'exoplanets_mass.txt'
+    """
     def funct(x): return float(
         x['pl_masse']) if x['pl_masse'] else 999_999_999_999
     data = sort_data(funct)
@@ -237,14 +294,18 @@ def exoplanets_mass():  # pl_masse
                 mass_tons = (float(exoplanet['pl_masse']) * 5.972e24) / 1000
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n"
-                    f"\tMass (M⊕​): {float(exoplanet['pl_masse']):,.2f} M⊕​ \n"
-                    f"\tMass (tons): {mass_tons:,.0f} tons \n")
+                    f"\tMass (M⊕​): {float(exoplanet['pl_masse']):,.2f} \n"
+                    f"\tMass (tons): {mass_tons:,.0f} \n")
             else:
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n\tNo data \n")
 
 
 def stars_mass():  # st_mass
+    """
+    Sort and enumerate the data by exoplanet's Star mass and formatted in Stellar Mass (M☉),
+    Planet mass (M⊕) and mass in tons and save it in 'exoplanets_star_mass.txt'
+    """
     def funct(x): return float(
         x['st_mass']) if x['st_mass'] else 999_999_999_999
     data = sort_data(funct)
@@ -257,9 +318,9 @@ def stars_mass():  # st_mass
                 planet_mass = mass_tons / (5.972e24 / 1000)
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n"
-                    f"\tStellar Mass (M☉): {float(exoplanet['st_mass']):,.3f} M☉​ \n"
+                    f"\tStellar Mass (M☉): {float(exoplanet['st_mass']):,.3f} \n"
                     f"\tMass (Tons): {mass_tons:,.0f} tons \n"
-                    f"\tPlanet Mass (M⊕​): {planet_mass:,.0f} M⊕​ \n")
+                    f"\tPlanet Mass (M⊕​): {planet_mass:,.0f} \n")
 
             else:
                 file.write(
@@ -267,6 +328,10 @@ def stars_mass():  # st_mass
 
 
 def insolation():  # pl_insol
+    """
+    Sort and enumerate the data by Incident Stellar Flux and formatted in Insolation (S⊕)
+    and Incident Stellar Flux (W/m²​) and save it in 'exoplanets_insolation.txt'
+    """
     def funct(x): return float(
         x['pl_insol']) if x['pl_insol'] else 999_999_999_999
     data = sort_data(funct)
@@ -278,8 +343,8 @@ def insolation():  # pl_insol
                 wm2 = float(exoplanet['pl_insol']) * 1361
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n"
-                    f"\tInsolation (S⊕): {float(exoplanet['pl_insol']):,.4f} S⊕​ \n"
-                    f"\tIncident Stellar Flux (W/m²​): {wm2:,.2f} W/m² \n")
+                    f"\tInsolation (S⊕): {float(exoplanet['pl_insol']):,.4f} \n"
+                    f"\tIncident Stellar Flux (W/m²​): {wm2:,.2f} \n")
             else:
                 file.write(
                     f"{i}) Name: {exoplanet['pl_name']}: \n\tNo data \n")
